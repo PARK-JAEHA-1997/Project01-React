2e7f7c571ff439fba990a4013ff9ba5f465f0390
Park Jaeha <parkjaeha97@gmail.com>
2023-04-14 19:38:22 +0900

    객체의 비교를 통해 알아보는 객체의 특성

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,36 +1,48 @@
 import React, { useState, useEffect } from "react";
 
-const TextView = React.memo(({ text }) => {
-  //React.memo로 인해 text가 변경될 때만 리렌더링된다
+const CounterA = React.memo(({ count }) => {
+  //A버튼은 눌러도 count가 그대로다 : 1에서 1로 바뀜 = 변경된 것이 없음
   useEffect(() => {
-    console.log(`Update :: Text : ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const CountView = React.memo(({ count }) => {
-  //React.memo로 인해 count가 변경될 때만 리렌더링된다
-  useEffect(() => {
-    console.log(`Update :: Count : ${count}`);
+    console.log(`CounterA Update - count : ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = React.memo(({ obj }) => {
+  //porp이 객체이기 때문에 (얕은 비교) 변화된다
+  useEffect(() => {
+    console.log(`CounterB Update - count : ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+});
+
 const OptimizeTest = () => {
   //컴포넌트를 재사용하는 실습용
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
+
+  const [count, setCount] = useState(1);
+  const [obj, setObj] = useState({
+    count: 1,
+  });
+
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
       </div>
       <div>
-        <h2>text</h2>
-        <TextView text={text} />
-        <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        <h2>Counter B</h2>
+        <CounterB obj={obj} />
+        <button
+          onClick={() =>
+            setObj({
+              count: obj.count,
+            })
+          }
+        >
+          B Button
+        </button>
       </div>
     </div>
   );
